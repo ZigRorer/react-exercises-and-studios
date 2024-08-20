f81c4bdf6bc842f20c7b0c1ac7d76b2400532a13
Admin <admin@Admins-MacBook-Air.local>
2024-08-19 21:35:08 -0400

    Exercises updated

--- part1/exercises/src/components/BookList.jsx
+++ part1/exercises/src/components/BookList.jsx
@@ -1,15 +1,15 @@
 export default function BookList() {
-   let pageTitle = "shrug";
-   let book1 = "google.com";
-   let book2 = "";
-   let book3 = "";
+   let pageTitle = "Coming Soon to a Book Shelf Near You";
+   let book1 = "https://images1.penguinrandomhouse.com/cover/9780593727935";
+   let book2 = "https://images3.penguinrandomhouse.com/cover/9781984805881";
+   let book3 = "https://images1.penguinrandomhouse.com/cover/9780593733257";
 
    return (
       <div>
          <h3>{pageTitle}</h3>
-         <img src={book1} alt="Nothing here!" />
-         <img src={book2} alt="Nothing here!" />
-         <img src={book3} alt="Nothing here!" />
+         <img src={book1} alt="The Plan by Kendra Adachi" />
+         <img src={book2} alt="The Village Library Demon-Hunting Society by C. M. Waggoner" />
+         <img src={book3} alt="A Sunny Place for Shady People by Mariana Enriquez" />
       </div>      
    );
 }
---
+++ part1/exercises/src/components/HobbyLinks.jsx
@@ -0,0 +1,7 @@
+function HobbyLinks() {
+    let hobbyLinks = ["https://www.lego.com/en-us/themes/animal-crossing", "https://animalcrossing.nintendo.com/"];
+    return(
+        <a href = {hobbyLinks[0]}>Animal Crossing Lego Sets</a>,
+        <a href = {hobbyLinks[1]}>Animal Crossing for Nintendo Switch</a>
+    );
+}
